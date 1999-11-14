77c631c6674f71069a32936fce893f9c9a5af5ac
dCache Random Developer <support@dcache.org>
1999-11-14 20:47:43 +0000

    client <-> store quere requests.

--- vehicles/StoreRequest.java
+++ vehicles/StoreRequest.java
@@ -8,6 +8,10 @@ public class StoreRequest implements java.io.Serializable {
    private String _store     = null ;
    private String _bfidName  = null ;
    private BitfileId _bfid   = null ; 
+   private String    _retMsg = "" ;
+   private int    _retCode   = 0 ;
+   private String _host      = null ;
+   private int    _port      = 0 ;
    public StoreRequest( String command ,
                         String id ,
                         String store ,
@@ -23,5 +27,18 @@ public class StoreRequest implements java.io.Serializable {
    public String getStore(){ return _store ; }
    public String getId(){ return _id ; }
    public String getCommand(){ return _command ; }
-   public String getBfid(){ return _bfidName ; } 
+   public String getBfid(){ return _bfidName ; }
+   public String getVolume(){ return _bfidName ; }
+   public String getHost(){ return _host ; }
+   public int    getPort(){ return _port ; }
+   public void   setHost( String host , int port ){
+      _host = host ;
+      _port = port ;
+   }
+   public String getReturnMessage(){ return _retMsg ; }
+   public int    getReturnCode(){ return _retCode ; }
+   public void   setReturnValue( int retCode , String retMsg ){
+       _retCode = retCode ;
+       _retMsg  = retMsg ;
+   } 
 }
