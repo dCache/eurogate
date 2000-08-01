2d26d5178ea9e450de74a27ce8b2fdea30c253ff
dCache Random Developer <support@dcache.org>
2000-08-01 16:17:16 +0000

    draws a one line border around the panel.

---
+++ spy/SimpleBorderPanel.java
@@ -0,0 +1,28 @@
+package eurogate.spy ;
+
+import java.awt.* ;
+
+public class SimpleBorderPanel extends Panel {
+   private int   _b     = 5 ;
+   private Color _color = Color.black ;
+   public SimpleBorderPanel( LayoutManager lo ){ super(lo) ; }
+   public SimpleBorderPanel( LayoutManager lo , int borderSize){ 
+      super(lo) ; 
+      _b = borderSize ;
+   }
+   public SimpleBorderPanel( LayoutManager lo , int borderSize , Color color ){ 
+      super(lo) ; 
+      _b = borderSize ;
+      _color = color ;
+   }
+   public Insets getInsets(){ return new Insets(_b , _b ,_b , _b) ; }
+   public void paint( Graphics g ){
+
+      Dimension   d    = getSize() ;
+      Color base = getBackground() ;
+      g.setColor( _color ) ;
+      g.drawRect( _b/2 , _b/2 , d.width-_b , d.height-_b ) ;
+   }
+
+
+}
