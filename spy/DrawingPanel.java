d4bf19f56cd3fcfd2c2c6645de3db2497fa32a03
dCache Random Developer <support@dcache.org>
2000-08-09 17:19:18 +0000

    multi moving pigs.

--- spy/DrawingPanel.java
+++ spy/DrawingPanel.java
@@ -9,20 +9,35 @@ import dmg.cells.applets.login.* ;
 public class      DrawingPanel
        extends    MasterPanel       {
    
+    private static final int IDLE    = 0 ;
+    private static final int INITIAL = 1 ;
+    private static final int MOVING  = 2 ;
+    private static final int FINAL   = 3 ;
+    private Font   _memberFont =  new Font( "SansSerif" , 0 , 12 ) ;
     private class CanvasPanel 
             extends Canvas 
             implements MouseListener , MouseMotionListener {
         private Image  _groupIcon =  null ;
         private Image  _userIcon =  null ;
         private int    _moving   = -1 ;
-        private int    _counter = 0 , _recPosX = 0 , _recPosY = 0 ;
+        private int    _state = 0 ;
+        private int    _recPosX = 0 , _recPosY = 0 ;
         private int    _dX =  0 , _dY = 0 ;
-        private Color  _background = new Color(200,200,200) ;
+        private Color  _background = new Color(140,140,200) ;
         private Vector _icon = new Vector() ;
         
+        
         private class Element {
-           private Point getPosition(){ return null ; }
-           private Rectangle getRectangle(){ return null ; }
+           private Rectangle _rectangle = new Rectangle(0,0,40,40) ;
+           private Image     _image     = null ;
+           private String    _name      = "" ;
+           private Element( String name , Image image ){ 
+              _name  = name ;
+              _image = image ; 
+           }
+           private Rectangle getRectangle(){ return _rectangle ; }
+           private String getName(){ return _name ; }
+           private Image getImage(){ return _image ; }
         }
         private CanvasPanel(){
            addMouseListener(this);
@@ -30,6 +45,7 @@ public class      DrawingPanel
         }
         private Image makeGroupIcon(){
            Image im = createImage(40,40) ;
+           if( im == null )System.out.println( "Image is null " ) ;
            Graphics g = im.getGraphics() ;
            Polygon p = new Polygon() ;
            p.addPoint( 5 , 35 ) ;
@@ -53,6 +69,7 @@ public class      DrawingPanel
         }
         private Image makeUserIcon(){
            Image im = createImage(40,40) ;
+           if( im == null )System.out.println( "Image is null " ) ;
            Graphics g = im.getGraphics() ;
            for( int i = 0 ; i < 40 ; i++ ){
               int red   = _background.getRed() ;
@@ -75,41 +92,49 @@ public class      DrawingPanel
             if( _groupIcon == null ){
                _groupIcon = makeGroupIcon() ;
                _userIcon  = makeUserIcon() ;
+               System.out.println( "setting icons "+_groupIcon+":"+_userIcon ) ;
+               _icon.addElement( new Element( "DESY" ,  _groupIcon ) ) ;
+               _icon.addElement( new Element( "Zeuthen" , _groupIcon ) ) ;
+               _icon.addElement( new Element( "Otto" , _userIcon ) ) ;
+               _icon.addElement( new Element( "Karl" , _userIcon ) ) ;
+               
             }
             refreshAll(g) ;
             update(g);
         }
         public void update( Graphics g ){
             
-            if( _moving > -1  ){
-               if( _counter == 0 ){
+           if( _moving > -1  ){
+            
+               Rectangle p     = ((Element)_icon.elementAt(_moving)).getRectangle() ;
+               Image     image = ((Element)_icon.elementAt(_moving)).getImage() ;
+               
+               if( _state == INITIAL ){
+                 //
                  // initial drawing of rectangle
+                 //
                  refreshAll(g) ;
-                 _counter ++ ;
                  g.setXORMode( Color.white ) ;
-                 _iconPosX = _recPosX ;
-                 _iconPosY = _recPosY ;
-                 g.drawImage( _groupIcon , _iconPosX , _iconPosY , null ) ;
+                 p.x = _recPosX ;
+                 p.y = _recPosY ;
+                 g.drawImage( image , p.x , p.y , null ) ;
                  g.setPaintMode() ;
-               }else if( _counter == -1 ) {
+                 _state = MOVING ;
+               }else if( _state == FINAL ) {
+                 p.x = _recPosX ;
+                 p.y = _recPosY ;
+                 _state = IDLE ;
                  _moving = -1 ;
+                 refreshAll(g) ;
+               }else if( _state == MOVING ){
                  g.setXORMode( Color.white ) ;
-                 _iconPosX = _recPosX ;
-                 _iconPosY = _recPosY ;
-                 g.drawImage( _groupIcon , _iconPosX , _iconPosY , null ) ;
-                 g.setPaintMode() ;
-                 g.drawImage( _groupIcon , _iconPosX , _iconPosY , null ) ;
-               }else{
-                 g.setXORMode( Color.white ) ;
-                 g.drawImage( _groupIcon , _iconPosX , _iconPosY , null ) ;
-                 _iconPosX = _recPosX ;
-                 _iconPosY = _recPosY ;
-                 g.drawImage( _groupIcon , _iconPosX , _iconPosY , null ) ;
+                 g.drawImage( image , p.x , p.y , null ) ;
+                 p.x = _recPosX ;
+                 p.y = _recPosY ;
+                 g.drawImage( image , p.x , p.y , null ) ;
                  g.setPaintMode() ;
                }
                
-            }else{
-               g.drawImage( _groupIcon , _iconPosX , _iconPosY , null ) ;
             }
             
         }
@@ -123,7 +148,25 @@ public class      DrawingPanel
             g.setColor(_background.darker());
             g.drawLine(0,d.height-1,d.width-1,d.height-1);
             g.drawLine(d.width-1,d.height-1,d.width-1,0);
-            g.drawImage( _userIcon , 50 , 50 , null ) ;
+            //
+            // now the pics
+            //
+            for( int i = 0 ; i < _icon.size() ; i++ ){
+              //
+              // we are not allowed to draw the moving pig in 
+              // paint mode.
+              //
+              if( i == _moving )continue ;
+              Element   e     = ((Element)_icon.elementAt(i)) ;
+              Image     image = e.getImage() ;
+              Rectangle rec   = e.getRectangle() ;
+              g.drawImage( image , rec.x ,rec.y , null ) ;
+              g.setColor(Color.black) ;
+              g.setFont( _memberFont ) ;
+              g.drawString( e.getName() , 
+                            rec.x + rec.width  + 4 ,
+                            rec.y + rec.height - 4 ) ;
+            }
         }
         public void mouseDragged(MouseEvent e){
           if( _moving > -1 ){
@@ -135,12 +178,12 @@ public class      DrawingPanel
         }
         public void mousePressed(MouseEvent e){
           Point p = e.getPoint() ;
-          for( int i = 0 ; i < _iconPos.size() ; i++ ){
+          for( int i = 0 ; i < _icon.size() ; i++ ){
              if( ((Element)_icon.elementAt(i)).getRectangle().contains(p) ){
-                 _moving = i ;
-                 _counter = 0 ;
-                 _dX = _icon.getPosition().x - e.getX() ;
-                 _dY = _icon.getPosition().y - e.getY() ;
+                 _moving  = i ;
+                 _state   = INITIAL ;
+                 _dX = ((Element)_icon.elementAt(i)).getRectangle().x - e.getX() ;
+                 _dY = ((Element)_icon.elementAt(i)).getRectangle().y - e.getY() ;
                  _recPosX = _dX + e.getX() ;
                  _recPosY = _dY + e.getY() ;
                  repaint() ;
@@ -152,7 +195,7 @@ public class      DrawingPanel
           if( _moving > -1 ){
             _recPosX = _dX + e.getX() ;
             _recPosY = _dY + e.getY() ;
-            _counter = -1 ;
+            _state = FINAL ;
             repaint() ;
           }
         }
